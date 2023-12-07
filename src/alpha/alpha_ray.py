6ebe1521a473fbd72ac03b7fc4266c751bf41771
M-Serrano-Altena <Marc.Serrano.Altena@gmail.com>
2023-12-07 21:45:54 +0100

    added the range of an alpha ray

--- src/alpha/alpha_ray.py
+++ src/alpha/alpha_ray.py
@@ -27,8 +27,11 @@ class Measurement:
 
     volt = []
     volt_error = []
+
     conversion_factor = 1 # placeholder value
+    energy_alpha_init = 5.48 # Mev
     energy = []
+
     energy_error = []
 
     stopping_power_list = []
@@ -124,11 +127,7 @@ class Measurement:
         Measurement.energy.append(self.volt_to_energy(self.fit_gauss1mu))
         Measurement.energy_error.append(self.volt_to_energy(self.fit_gauss1mu_err))
 
-    @classmethod
-    def energy_fit(cls):
-        """fit of the energy diagram with the exponential decay function
-        """        
-        def exp_decay(x, a: float, b:float, c: float) -> float:
+    def exp_decay(x, a: float, b:float, c: float) -> float:
             """Exponential decay function to fit for the (energy, path length) diagram
 
             Args:
@@ -142,22 +141,30 @@ class Measurement:
             """            
             energy = c - a * b**x
             return energy
-        
+
+    @classmethod
+    def energy_fit(cls):
+        """fit of the energy diagram with the exponential decay function
+        """        
         energy_weight = [1/energy_err for energy_err in cls.energy_error]
-        cls.model_energy = models.Model(exp_decay, nan_policy='propagate')
+        cls.model_energy = models.Model(cls.exp_decay, nan_policy='propagate')
         cls.result_energy = cls.model_energy.fit(cls.energy, x=cls.path_length_list, weights=energy_weight, a=0.1, b=5, c=2)
         print(cls.result_energy.fit_report())
         cls.a = cls.result_energy.params['a'].value
         cls.b = cls.result_energy.params['b'].value
+        cls.c = cls.result_energy.params['c'].value
 
 
     @classmethod
     def energy_plot(cls):
         """Plot a diagram of the energy of the alpha particle against the path length 
-        """        
+        """
+        cls.path_length_continuous = np.arange(cls.path_length_list[0], cls.path_length_list[-1], 0.01)
+        cls.energy_continuous = [cls.exp_decay(num, a=cls.a, b=cls.b, c=cls.c) for num in cls.path_length_continuous]
+
         fig = plt.figure("(E,x)_diagram.png")
         plt.errorbar(cls.path_length_list, cls.energy, yerr=cls.energy_error, fmt='bo', ecolor='k', label='data')
-        plt.plot(cls.path_length_list, cls.result_energy.best_fit, 'r', label='best fit')
+        plt.plot(cls.path_length_continuous, cls.energy_continuous, 'r', label=f'E(x) = c - a*b^x')
         plt.xlabel('path length (cm)')
         plt.ylabel('Energy (MeV)')
         plt.legend(loc='upper right')
@@ -173,8 +180,7 @@ class Measurement:
     def calc_conversion_factor(self):
         """calculate the conversion factor to go from a voltage to an energy
         """        
-        energy_alfa = 5.48 # Mev
-        Measurement.conversion_factor = energy_alfa/self.fit_gauss1mu
+        Measurement.conversion_factor = Measurement.energy_alpha_init/self.fit_gauss1mu
 
     @staticmethod
     def volt_to_energy(voltage: float) -> float:
@@ -225,12 +231,16 @@ class Measurement:
     @classmethod
     def stopping_power_plot(cls):
         """Plots the stopping power against the path length and plots an (E,x) and a (S,x) diagram
-        """        
+        """
         cls.stopping_power_list = [cls.stopping_power_function(num) for num in cls.path_length_list]
+        cls.path_length_continuous = np.arange(cls.path_length_list[0], 4, 0.01)    
+        cls.stopping_power_continuous = [cls.stopping_power_function(num) if cls.exp_decay(num, a=cls.a, b=cls.b, c=cls.c) > 0 else 0 for num in cls.path_length_continuous]
         fig = plt.figure("(S,x)_diagram.png")
-        plt.plot(cls.path_length_list, cls.stopping_power_list, 'r')
+        plt.scatter(cls.path_length_list, cls.stopping_power_list, c='blue', label='Stopping power of measured data')
+        plt.plot(cls.path_length_continuous, cls.stopping_power_continuous, 'r', label=f'S(x) = dE/dx = ln(b)*a*b^x')
         plt.xlabel("path length (cm)")
         plt.ylabel("Stopping power (MeV/cm)")
+        plt.legend(loc='upper left')
 
         # go to fits folder to save fit
         os.chdir(os.path.join(python_file_path, "Fits"))
@@ -240,9 +250,15 @@ class Measurement:
         os.chdir(os.path.join(python_file_path, "CSV data"))
         plt.show()
     
+    @classmethod
+    def alpha_range(cls):
+        # we could also use c - E0 instead of a
+        range_alpha_ray = np.log(cls.c/(cls.a))/np.log(cls.b)
+        return range_alpha_ray
 
-def run():
-    """runs the experiment with different pressures
+
+def measurement_air():
+    """runs the experiment with different pressures in air
     """    
     meas1_vacuum = Measurement("alfa bron 21 mbar.csv", end_point=1000, pressure = 21)
     meas1_vacuum.data_fit(start_expmu=0.05, start_gauss1_mu=0.2)
@@ -287,6 +303,13 @@ def run():
     Measurement.energy_fit()
     Measurement.energy_plot()
     Measurement.stopping_power_plot()
+    print(f"The range of an alpha particle in air with atmosiferic pressure is {round(Measurement.alpha_range(), 2)} cm")
+
+
+def run():
+    """Runs measurements with different gasses
+    """    
+    measurement_air()
 
 
 if __name__ == "__main__":
